0d7c463bcc82d8934323b252f29884bceaf1f235
Ubuntu <ubuntu@ip-172-31-44-84.us-east-2.compute.internal>
2026-03-06 14:47:44 +0000

    CI/CD test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps! Flask app is running."
+    return "Hello from DevOps Flask App! CI/CD test."
 
 @app.route("/health")
 def health():
